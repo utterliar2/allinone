588f14dadafa83b1c1222eb493b3e2f18a09d220
GitHub Action <action@github.com>
2021-11-10 08:28:30 +0000

    update

--- jd_price.js
+++ jd_price.js
@@ -44,6 +44,7 @@ const JD_API_HOST = 'https://api.m.jd.com/';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
+  await jstoken();
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
